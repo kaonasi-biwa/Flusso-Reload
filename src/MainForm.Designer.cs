3314d2b2f4e3274506428843905724469ffbd03c
kaonasi-biwa <kaonasi.biwa@gmail.com>
2022-06-18 22:34:16 +0900

    タイトルバーをFlussoからFlusso Reloadに変更

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -345,7 +345,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Flusso";
+            this.Text = "Flusso Reload";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
